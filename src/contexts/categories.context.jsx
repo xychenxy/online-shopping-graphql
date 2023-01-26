64e8b143fd81601f96a2eaeb11273f5b744952eb
xychenxy <mavischen916@gmail.com>
2023-01-26 21:54:03 +1100

    using graphql

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,51 @@
+import { createContext, useState, useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+export const CategoriesContext = createContext({
+	categoriesMap: {},
+});
+
+const COLLECTIONS = gql`
+	query {
+		collections {
+			id
+			title
+			items {
+				id
+				name
+				price
+				imageUrl
+			}
+		}
+	}
+`;
+
+export const CategoriesProvider = ({ children }) => {
+	const { loading, error, data } = useQuery(COLLECTIONS);
+	const [categoriesMap, setCategoriesMap] = useState({});
+
+	useEffect(() => {
+		if (data) {
+			const { collections } = data;
+			const collectionsMap = collections.reduce((acc, collection) => {
+				const { title, items } = collection;
+				acc[title.toLowerCase()] = items;
+				return acc;
+			}, {});
+			setCategoriesMap(collectionsMap);
+		}
+	}, [data]);
+
+	const value = { categoriesMap, loading };
+	return (
+		<>
+			{error ? (
+				<div>Something is going wrong</div>
+			) : (
+				<CategoriesContext.Provider value={value}>
+					{children}
+				</CategoriesContext.Provider>
+			)}
+		</>
+	);
+};
---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,62 @@
+import { useState, useEffect, Fragment } from "react";
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+import ProductCard from "../../components/product-card/product-card.component";
+
+import { CategoryContainer, Title } from "./category.styles";
+import Spinner from "../../components/spinner/spinner.component";
+
+const GET_CATEGORY = gql`
+	query ($title: String!) {
+		getCollectionsByTitle(title: $title) {
+			id
+			title
+			items {
+				id
+				name
+				price
+				imageUrl
+			}
+		}
+	}
+`;
+
+const Category = () => {
+	const { category } = useParams();
+	const { loading, data } = useQuery(GET_CATEGORY, {
+		variables: { title: category },
+	});
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		if (data) {
+			const {
+				getCollectionsByTitle: { items },
+			} = data;
+			setProducts(items);
+		}
+	}, [category, data]);
+
+	return (
+		<Fragment>
+			{loading ? (
+				<Spinner />
+			) : (
+				<>
+					<Title>{category.toUpperCase()}</Title>
+					<CategoryContainer>
+						{products &&
+							products.map((product) => (
+								<ProductCard
+									key={product.id}
+									product={product}
+								/>
+							))}
+					</CategoryContainer>
+				</>
+			)}
+		</Fragment>
+	);
+};
+
+export default Category;
